d7c6661114b6af8bc182c430bac62cebcdafe64c
ThalesAlmeida <thalesalves44@gmail.com>
2018-06-09 02:18:58 -0300

    'inicial'

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[]=[
+    {fraseEng: 'I like to learn' , frasePtBr:'eu gosto de aprender'},
+    {fraseEng: 'I love you' , frasePtBr:'eu amo você'},
+    {fraseEng: 'I watch tv at home' , frasePtBr:'eu assisto tv em casa'},
+    {fraseEng: 'I have to study more' , frasePtBr:'eu tenho que estudar mais'}
+]
+
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model'
+import {FRASES} from  './frases-mock'
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit {
+  
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase'
+  public resposta:string = ""
+  
+  public rodada: number = 0
+  public rodadaFrase:Frase
+
+  public progresso:number = 0
+
+  public tentativas: number = 4
+
+  
+  constructor() {
+    this.atualizaRodada()
+    
+    //console.log(this.rodadaFrase)
+  }
+
+  ngOnInit() {
+  }
+
+ public atualizaResposta(resposta:Event): void{
+   this.resposta = (<HTMLInputElement>resposta.target).value
+   //console.log(this.resposta)
+   
+ }
+
+ public verificarResposta():void{
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert("A tradução está correta")
+
+      this.rodada++
+      this.progresso = this.progresso +(100/this.frases.length)
+      console.log(this.progresso)
+
+      if(this.rodada === 4){
+        alert("Concluiu com sucesso, parabens, c é o bichão memo hein")
+      }
+
+      this.atualizaRodada()
+
+    } else{
+      this.tentativas--
+
+      if(this.tentativas === -2){
+        alert("Já era, meu irmão")
+      }
+    }
+    
+ }
+
+ public atualizaRodada():void{
+    this.rodadaFrase = this.frases[this.rodada]
+    this.resposta = ''
+ }
+}
